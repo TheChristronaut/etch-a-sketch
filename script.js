8a3a319ae641d48afc1a57b487ecfcbc6ab3b0c6
Zach <zswiger1997@gmail.com>
2024-03-22 09:02:52 -0500

    Add drawing method and grid size prompt

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+const gridSize = prompt("Enter your grid size");
+
 function generateGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const container = document.querySelector("#container");
         const line = document.createElement("div");
-
         line.setAttribute("id", "line" + i);
         line.classList.add("line")
-
         container.appendChild(line);
-
         function generateLine() {
-            for (let j = 0; j < 16; j++){
+            for (let j = 0; j < gridSize; j++){
                 const line = document.querySelector("#line" + i);
                 const box = document.createElement("div");
-
                 box.classList.add("one-square");
                 box.style.cssText = "background: white;";
-
+                box.addEventListener("mouseover", () => {
+                    box.style.cssText = "background: black;";
+                })
                 line.appendChild(box);
                 console.log("This function works");
             }
